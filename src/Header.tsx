356a6497213cc825be037f8e97d5a89c135b569b
jdehorty <justin.dehorty@bentley.com>
2021-02-17 14:24:50 -0500

    Update Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,39 +1,39 @@
-import { Button, ButtonType } from "@bentley/ui-core";
+import {BlockText, Button, ButtonType} from "@bentley/ui-core";
 import React from "react";
 
 import styles from "./Header.module.scss";
 
 interface HeaderProps {
-  handleLogin: () => void;
-  handleLogout: () => void;
-  loggedIn: boolean;
+    handleLogin: () => void;
+    handleLogout: () => void;
+    loggedIn: boolean;
 }
 
-export const Header: React.FC<HeaderProps> = ({
-  loggedIn,
-  handleLogin,
-  handleLogout,
-}: HeaderProps) => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.buttonContainer}>
-        <Button
-          className={styles.button}
-          onClick={handleLogin}
-          buttonType={ButtonType.Primary}
-          disabled={loggedIn}
-        >
-          {"Sign In"}
-        </Button>
-        <Button
-          className={styles.button}
-          onClick={handleLogout}
-          buttonType={ButtonType.Primary}
-          disabled={!loggedIn}
-        >
-          {"Sign Out"}
-        </Button>
-      </div>
-    </header>
-  );
+export const Header = (props: HeaderProps) => {
+    const {loggedIn, handleLogin, handleLogout,} = props;
+    const {button, text, buttonContainer, header} = styles;
+
+    return (
+        <header className={header}>
+            <div className={buttonContainer}>
+                <Button className={button} onClick={handleLogin} buttonType={ButtonType.Primary}
+                        disabled={loggedIn}>
+                    {"Sign In"}
+                </Button>
+                <Button className={button} onClick={handleLogout} buttonType={ButtonType.Primary}
+                        disabled={!loggedIn}>
+                    {"Sign Out"}
+                </Button>
+            </div>
+            <div>
+                <BlockText
+                    className={text}
+                >
+                    {"ML Labeler"}
+                </BlockText>
+            </div>
+        </header>
+    );
 };
+
+
