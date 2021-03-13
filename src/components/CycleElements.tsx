ca7d79eb899e53f8971610b32f5c0e36a4fade4d
BillyDJr <bill.dehorty@gmail.com>
2021-03-13 13:40:40 -0600

    Replace Fast-backward, back-frame, fwd-frame, and fast forward with Svg.

--- src/components/CycleElements.tsx
+++ src/components/CycleElements.tsx
@@ -106,8 +106,33 @@ export class CycleElementComponent extends React.Component<CycleElementComponent
                     {
                         this.props.enabled &&
                         <>
-                            <Button className="cycler-button" disabled={this.props.working || !this.props.ready} onClick={()=>this.props.onBackward(-fastCount)}><Icon iconSpec="icon-media-controls-fast-backward" /></Button>
-                            <Button className="cycler-button" disabled={this.props.working || !this.props.ready} onClick={()=>this.props.onBackward(-1)}><Icon iconSpec="icon-media-controls-frame-backward" /></Button>
+                            <Button className="cycler-button" disabled={this.props.working || !this.props.ready} 
+                                     style={{width: '24px', height: '22px'}}
+                                     onClick={()=>this.props.onBackward(-fastCount)}>
+                                {/*<Icon iconSpec="icon-media-controls-fast-backward" />*/}
+                                <SvgPath viewBoxWidth={16} viewBoxHeight={16}  paths={[
+                                    "m14.2222 4a.76016.76016 0 0 1 .3904.1081.80441.80441 0 0 1 " + 
+		                            ".3874.6919v6.4a.80441.80441 0 0 1 -.3874.6919.75908.75908 0 0 1 " + 
+		                            "-.7763.0027l-5.4444-3.2a.8115.8115 0 0 1 0-1.3892l5.4444-3.2a.76069.76069 0 0 1 " + 
+		                            ".3859-.1054zm-7 0a.76016.76016 0 0 1 .3904.1081.80441.80441 0 0 1 " + 
+		                            ".3874.6919v6.4a.80441.80441 0 0 1 -.3874.6919.75908.75908 0 0 1 " + 
+		                            "-.7763.0027l-5.4444-3.2a.8115.8115 0 0 1 0-1.3892l5.4444-3.2a.76069.76069 0 0 1 .3859-.1054z"
+                                    ]}
+                                    />
+                            </Button>
+                            <Button className="cycler-button" 
+                                    disabled={this.props.working || !this.props.ready} 
+                                    style={{width: '24px', height: '22px'}}
+                                    onClick={()=>this.props.onBackward(-1)}>
+                                {/* <Icon iconSpec="icon-media-controls-frame-backward" /> */}
+                                <SvgPath viewBoxWidth={16} viewBoxHeight={16}  paths={[
+                                        "m9.2222 4a.76016.76016 0 0 1 .3904.1081.80441.80441 0 0 1 .3874.6919v6.4a.80441.80441 0 0 1" + 
+                                        " -.3874.6919.75908.75908 0 0 1 -.7763.0027l-5.4444-3.2a.8115.8115 0 0 1 " + 
+                                        "0-1.3892l5.4444-3.2a.76069.76069 0 0 1 .3859-.1054zm2.7778-1a1 1 0 0 1 1 " + 
+                                        "1v8a1 1 0 0 1 -1 1 1 1 0 0 1 -1-1v-8a1 1 0 0 1 1-1z"
+                                    ]}
+                                />
+                            </Button>
                             <div className="cycler-progress">
                                 <div className="cycler-title">{IModelApp.i18n.translate("LabelingApp:cycler.cyclingTitle")}</div>
                                 <div className="cycler-value">
@@ -118,8 +143,34 @@ export class CycleElementComponent extends React.Component<CycleElementComponent
                                     {`${this.props.cycleSetSize!}`}
                                 </div>
                             </div>
-                            <Button className="cycler-button" disabled={this.props.working || !this.props.ready} onClick={()=>this.props.onForward(1)}><Icon iconSpec="icon-media-controls-frame-forward" /></Button>
-                            <Button className="cycler-button" disabled={this.props.working || !this.props.ready} onClick={()=>this.props.onForward(fastCount)}><Icon iconSpec="icon-media-controls-fast-forward" /></Button>
+                            <Button className="cycler-button" 
+                                    disabled={this.props.working || !this.props.ready} 
+                                    style={{width: '24px', height: '22px'}}
+                                    onClick={()=>this.props.onForward(1)}>
+                                        {/* <Icon iconSpec="icon-media-controls-frame-forward"/> */}
+                                        <SvgPath viewBoxWidth={16} viewBoxHeight={16}  paths={[
+                                        "m6.7778 4a.76016.76016 0 0 0 -.3904.1081.80441.80441 0 0 0 -.3874.6919v6.4a.80441.80441 0 0 0 " + 
+		                                ".3874.6919.75908.75908 0 0 0 .7763.0027l5.4444-3.2a.8115.8115 0 0 0 " + 
+		                                "0-1.3892l-5.4444-3.2a.76069.76069 0 0 0 -.3859-.1054zm-2.7778-1a1 1 0 0 0 " + 
+		                                "-1 1v8a1 1 0 0 0 1 1 1 1 0 0 0 1-1v-8a1 1 0 0 0 -1-1z"
+                                        ]}
+                                        />
+                                      
+                            </Button>
+                            <Button className="cycler-button" 
+                                    disabled={this.props.working || !this.props.ready} 
+                                    style={{width: '24px', height: '22px'}}
+                                    onClick={()=>this.props.onForward(fastCount)}>
+                                        {/*<Icon iconSpec="icon-media-controls-fast-forward" />*/}
+                                        <SvgPath viewBoxWidth={16} viewBoxHeight={16}  paths={[
+                                       "m1.7778 4a.76016.76016 0 0 0 -.3904.1081.80441.80441 0 0 0 -.3874.6919v6.4a.80441.80441 0 0 0 " + 
+                                       ".3874.6919.75908.75908 0 0 0 .7763.0027l5.4444-3.2a.8115.8115 0 0 0 0-1.3892l-5.4444-3.2a.76069.76069 " + 
+                                       "0 0 0 -.3859-.1054zm7 0a.76016.76016 0 0 0 -.3904.1081.80441.80441 0 0 0 -.3874.6919v6.4a.80441.80441 0 0 0 " + 
+                                       ".3874.6919.75908.75908 0 0 0 .7763.0027l5.4444-3.2a.8115.8115 0 0 0 0-1.3892l-5.4444-3.2a.76069.76069 0 0 0 " + 
+                                       "-.3859-.1054z"
+                                    ]}
+                                    />
+                            </Button>
                             <Button className="cycler-button" disabled={this.props.working || !this.props.ready} onClick={this.props.onStop}><Icon iconSpec="icon-media-controls-stop" /></Button>
                         </>
                     }
